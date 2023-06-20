0b8d11563c2ffe35e1753601e5f44776c1923b05
Chamaththa Shamod <shamodchamaththa@gmail.com>
2023-06-20 18:16:13 +0530

    product init

--- ITP-Cake-Shop-Management-System-main/client/src/components/Product/Product.js
+++ ITP-Cake-Shop-Management-System-main/client/src/components/Product/Product.js
@@ -5,8 +5,63 @@ import Sidebar from '../../components/Layout/Sidebar'
 import AddProduct from './AddProduct'
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import { Table } from "react-bootstrap";
+import { MDBInput } from "mdb-react-ui-kit";
+import Rating from "@mui/material/Rating";
 
 function Product() {
+
+  const [oitems, setoitems] = useState([]);
+  console.log(oitems);
+
+  const id = useParams().id;
+  console.log(id);
+
+  useEffect(() => {
+    getOitems();
+  }, [id]);
+  const getOitems = () => {
+    axios
+      .get("http://localhost:8000/")
+      .then((res) => {
+        setoitems(res.data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  const [search, setSearch] = useState("");
+  const searchItem = oitems.filter((oitems) => {
+    if (search === "") {
+      return oitems;
+    } else if (
+      oitems.item_name.toLowerCase().includes(search.toLocaleLowerCase()) ||
+      oitems.item_code.toLowerCase().includes(search.toLocaleLowerCase())
+    ) {
+      return oitems;
+    }
+  });
+
+  function Button({ status }) {
+    return (
+      <p
+        style={{
+          color: status === "Out Of Stock" ? "red" : "green",
+          backgroundColor: status === "Out Of Stock" ? "#FFBEBE " : "#8EFF94",
+          width: "100px",
+          borderRadius: "16px",
+          height: "22px",
+          fontSize: "14px",
+          fontWeight: "600",
+          textAlign: "center",
+        }}
+      >
+        {status}
+      </p>
+    );
+  }
+
 
     const [oitems_Count, setoitems_Count] = useState([]);
     useEffect(() => {
@@ -84,7 +139,104 @@ function Product() {
       className="mb-3"
     >
       <Tab eventKey="home" title="All Products">
-        Tab content for Home
+      <MDBInput
+        className="mt-2 bg-white"
+        placeholder="Search Other Item..."
+        style={{
+          width: "20%",
+          borderRadius: "20px",
+          float: "left",
+          marginLeft: "64%",
+        }}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button
+        style={{
+          marginTop: "12px",
+          marginLeft: "-35px",
+          borderRadius: "50%",
+          backgroundColor: "#B666D2 ",
+          border: "none",
+          width: "30px",
+          height: "30px",
+        }}
+      >
+        <i class="fa fa-search" style={{ color: "white" }}></i>
+      </button>
+      <Table
+                class="table table-bordered "
+                striped
+                style={{ width: "98%" ,margin:'10px'}}
+              >
+                <thead>
+                  <tr className="table-dark">
+                    <th scope="col">Id</th>
+                    <th scope="col">Item Code</th>
+                    <th scope="col">Item Name</th>
+                    <th scope="col">Item Category</th>
+                    <th scope="col">Rating</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Price</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {searchItem.map((oitems, id) => {
+                    return (
+                      <>
+                        <tr style={{ borderStyle: "dotted", fontSize: "15px" }}>
+                          <th scope="row" style={{ borderStyle: "dotted" }}>
+                            {id + 1}
+                          </th>
+
+                          <td style={{ borderStyle: "dotted" }}>
+                            <a
+                              href={`/oitem/employee/${oitems._id}`}
+                              style={{ textDecoration: "none", color: "black" }}
+                            >
+                              {oitems.item_code}
+                            </a>
+                          </td>
+                          <td style={{ borderStyle: "dotted" }}>
+                            <a
+                              href={`/oitem/employee/${oitems._id}`}
+                              style={{ textDecoration: "none", color: "black" }}
+                            >
+                              {oitems.item_name}
+                            </a>
+                          </td>
+                          <td style={{ borderStyle: "dotted" }}>
+                            {oitems.category}
+                          </td>
+                          <td style={{ borderStyle: "dotted" }}>
+                           <Rating
+                              style={{ fontSize: "17px", marginLeft: "7px" }}
+                              name="half-rating-read"
+                              defaultValue={oitems.avgRating}
+                              precision={0.5}
+                              readOnly
+                            />  
+                          </td>
+                          <td
+                            style={{ borderStyle: "dotted" }}
+                            color="success"
+                            pill
+                          >
+                            <Button
+                              status={oitems.status}
+                              style={{ width: "10px", float: "left" }}
+                            />
+                          </td>
+                          <td style={{ borderStyle: "dotted" }}>
+                            Rs.{oitems.price.toFixed(2)}
+                          </td>
+                        <td><a  href={`/product/admin/${oitems._id}`} style={{textDecoration:'none'}}>See More</a></td>
+                        </tr>
+                      </>
+                    );
+                  })}
+                </tbody>
+              </Table>
       </Tab>
       <Tab eventKey="profile" title="Add New Product">
       <AddProduct/>
