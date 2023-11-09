991171c7b35532c03069b8db31a94920fcb18300
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-09 20:24:07 +0530

    Update Login.js

--- ITP-Cake-Shop-Management-System-main/client/src/pages/Auth/Login.js
+++ ITP-Cake-Shop-Management-System-main/client/src/pages/Auth/Login.js
@@ -5,6 +5,7 @@ import { useAuth } from "../../context/auth";
 import Layout from "../../components/Layout/Layout";
 import Footer from "../../components/Layout/Footer";
 import Header from "../../components/Layout/Header";
+import Card from 'react-bootstrap/Card';
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -42,6 +43,7 @@ const Login = () => {
     
       <Layout>
         <Header/>
+         
         <center>
           
         <div style={{backgroundColor:'white'}}>
@@ -49,10 +51,10 @@ const Login = () => {
           <br></br>
         <div className="form-container " style={{width:'30%',textAlign:'left'}}>
           <br></br>
-          <br></br>
-          <br></br>
-        <form onSubmit={handleSubmit}>
-         <center> <h4 className="title">LOGIN</h4></center>
+          <Card style={{padding:'30px'}}>
+          <form onSubmit={handleSubmit}>
+         <center> <i><h4 className="title">Signin to Cake Fantasy</h4></i></center>
+         <br/>
 
           <div className="mb-3">
             <input
@@ -82,12 +84,12 @@ const Login = () => {
                 navigate("/ForgotPassword");
               }} href="" style={{textDecoration:'none'}}>Forgot Password ?</a>
           </div>
-          <button type="submit" className="btn btn-primary" style={{width:'100%',textAlign:'center'}}>
-            LOGIN
+          <button type="submit" className="btn btn-primary" style={{width:'100%',textAlign:'center',backgroundColor:'#b666d2',borderColor:"#b666d2"}}>
+            SignIn
           </button>
         </form>
-        <br></br>
-        <br></br>
+    </Card>
+
         <br></br>
         <br></br>
         
